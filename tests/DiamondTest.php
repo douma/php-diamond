461db87b9e65be8c18260ab9131288c78956d932
Stefan Douma <douma4@gmail.com>
2019-06-14 00:45:26 +0200

    Init

---
+++ tests/DiamondTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class DiamondTest extends TestCase
+{
+    public function test(): void
+    {
+        new Diamond;
+    }
+}
